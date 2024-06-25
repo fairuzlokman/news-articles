57a0eeea5f29eb52bc21d35aa0eda6dcc264367a
Fairuz Lokman <fairuzlokman99@gmail.com>
2024-06-26 05:19:19 +0800

    wip: mobile responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,13 +44,26 @@ const Header = () => {
 	return (
 		<div className="relative z-50 flex items-center justify-between px-[30px] py-4 bg-white shadow-md">
 			<Link href={"/"}>
-				<i>The Old Newspaper</i>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					className="size-6 sm:hidden"
+				>
+					<path
+						fillRule="evenodd"
+						d="M4.125 3C3.089 3 2.25 3.84 2.25 4.875V18a3 3 0 0 0 3 3h15a3 3 0 0 1-3-3V4.875C17.25 3.839 16.41 3 15.375 3H4.125ZM12 9.75a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H12Zm-.75-2.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75ZM6 12.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5H6Zm-.75 3.75a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5H6a.75.75 0 0 1-.75-.75ZM6 6.75a.75.75 0 0 0-.75.75v3c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-3A.75.75 0 0 0 9 6.75H6Z"
+						clipRule="evenodd"
+					/>
+					<path d="M18.75 6.75h1.875c.621 0 1.125.504 1.125 1.125V18a1.5 1.5 0 0 1-3 0V6.75Z" />
+				</svg>
+				<i className="hidden sm:block">The Old Newspaper</i>
 			</Link>
 			{isSearching ? (
 				<div className="absolute right-[30px] flex items-center gap-2">
 					<form
 						onSubmit={handleSubmit}
-						className="flex items-center pr-3 border rounded-md border-input"
+						className="flex items-center pr-3 border rounded-md border-black/50"
 					>
 						<Input
 							value={search}
@@ -86,15 +99,17 @@ const Header = () => {
 				</div>
 			) : (
 				<div className="flex gap-[30px] transition-all">
-					{searchSuggestions.map((item, index) => (
-						<Link
-							key={index}
-							href={item}
-							className="text-sm hover:underline hover:underline-offset-2"
-						>
-							{item}
-						</Link>
-					))}
+					<div className="lg:flex gap-[30px] hidden">
+						{searchSuggestions.map((item, index) => (
+							<Link
+								key={index}
+								href={item}
+								className="text-sm hover:underline hover:underline-offset-2"
+							>
+								{item}
+							</Link>
+						))}
+					</div>
 					<button onClick={() => setIsSearching(true)}>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
